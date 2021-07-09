4fede779d27bcfaa40e0e9d06be43e9f12fae69c
Kolgolar <62188763+Kolgolar@users.noreply.github.com>
2021-07-09 15:45:27 +0300

    тварь

--- Waybill/app_main/static/app_main/js/app_main-index.js
+++ Waybill/app_main/static/app_main/js/app_main-index.js
@@ -1,15 +1,14 @@
 ﻿function addForm() {
-    alert("YES");
     let formClone = $("#form_to_clone").clone();
     formClone.appendTo($("#paste_form_here"));
 }
 
 function removeForm() {
-
+    $("#paste_form_here").last().remove();
 }
 
 
 $(document).ready(function () {
-    $("#b_add").click(addForm());
-    $("#b_remove").click(removeFrom());
+    $("#b_add").click(addForm);
+    $("#b_remove").click(removeForm);
 })
