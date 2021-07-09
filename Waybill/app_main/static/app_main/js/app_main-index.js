bb1a3d3c15d3bf5a757971739828ce899dc3b392
Kolgolar <62188763+Kolgolar@users.noreply.github.com>
2021-07-09 15:05:01 +0300

    in-dev...

--- Waybill/app_main/static/app_main/js/app_main-index.js
+++ Waybill/app_main/static/app_main/js/app_main-index.js
@@ -1,1 +1,15 @@
-﻿
+﻿function addForm() {
+    alert("YES");
+    let formClone = $("#form_to_clone").clone();
+    formClone.appendTo($("#paste_form_here"));
+}
+
+function removeForm() {
+
+}
+
+
+$(document).ready(function () {
+    $("#b_add").click(addForm());
+    $("#b_remove").click(removeFrom());
+})
